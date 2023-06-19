ab17588367ac273eb9bab692dc8b93df0b4e7e8d
Your Name <09taumang@gmail.com>
2023-06-19 12:20:16 +0200

    Added Error Boundaries to catch errors from the LogininPage

--- beekeeping_inspection_checklist/src/App.jsx
+++ beekeeping_inspection_checklist/src/App.jsx
@@ -1,19 +1,48 @@
 import './App.css'
 import Navbar from './Navbar/navbar'
-
+import LoginPage from './Login__Register/login__register'
 import BeekeepingInspectionChecklist from './Bee checklist/bee_checklist_main'
 
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    // Update state to indicate an error has occurred
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // You can also log the error or send it to an error reporting service
+    console.error(error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // Render fallback UI when an error occurs
+      return <h1>Something went wrong.</h1>;
+    }
+
+    // Render the child components as-is
+    return this.props.children;
+  }
+}
+
+
 function App() {
-  
   return (
     <div className='App'>
-      <Navbar/>
-        <div className='container'>
-          
-          <BeekeepingInspectionChecklist/>
-        </div>
+      {/* <Navbar /> */}
+      <div className='container'>
+        <ErrorBoundary>
+          <LoginPage />
+        </ErrorBoundary>
+        {/* <BeekeepingInspectionChecklist /> */}
+      </div>
     </div>
-  )
+  );
 }
 
 export default App
--- beekeeping_inspection_checklist/src/Navbar/navbar.jsx
+++ beekeeping_inspection_checklist/src/Navbar/navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import LoginPage from "../Login__Register/login__register";
 
 
 const navBar = () => {
@@ -21,7 +22,7 @@ const navBar = () => {
     <nav className="flex items-center justify-between flex-wrap p-4 bg-yellow-500 text-white">
       <div className="flex items-center">
         <img
-          src="../assets/Another_one_logo_1020Hivezzz.png"
+          src="/beekeeping_inspection_checklist/src/assets/Another_one_logo_1020Hivezzz.png"
           alt="1020 Hivezzz logo"
           className="w-12"
         />
@@ -40,6 +41,7 @@ const navBar = () => {
           
         </button>
       </div>
+      
       <div className={`w-full flex-grow lg:flex lg:items-center lg:w-auto ${showMenu ? 'block' : 'hidden'}`}>
         <div className="text-sm lg:flex-grow flex justify-center">
           <a href="#responsive-header" className="block mt-4 lg:inline-block lg:mt-0 text-white hover:text-white mr-4">
@@ -66,7 +68,7 @@ const navBar = () => {
               className="bg-black px-4 py-2 rounded-full text-white hover:bg-white hover:text-black"
               onClick={handleLoginClick}
             >
-              Login
+              
             </button>
           )}
        
