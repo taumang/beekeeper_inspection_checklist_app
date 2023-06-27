28e325c3fe2adad36b7ef00e874a9e31216b78f5
Your Name <09taumang@gmail.com>
2023-06-28 01:39:59 +0200

    more changes on the layout of he login form

--- beekeeping_inspection_checklist/src/App.jsx
+++ beekeeping_inspection_checklist/src/App.jsx
@@ -1,20 +1,24 @@
 import './App.css'
 // import  {login}  from './pages/Login__Register/login'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { login_page } from './pages/login_page';
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Login_page  from './pages/login_page';
 
 
 
 
 const App = () => {
   return (
-    <Router>
-      <div className="min-h-screen bg-gray-100 flex justify-center items-center">
-        <Switch>
-          <Route path="/" exact component={login_page} />
-        </Switch>
-      </div>
-    </Router>
+    // <Router>
+    //   <div className="min-h-screen bg-gray-100 flex justify-center items-center">
+    //     <Switch>
+    //       <Route path="/" exact component={login_page} />
+    //     </Switch>
+    //   </div>
+    // </Router>
+
+    <>
+    <Login_page/>
+    </>
   );
 }
 
--- beekeeping_inspection_checklist/src/pages/login_page.jsx
+++ beekeeping_inspection_checklist/src/pages/login_page.jsx
@@ -1,30 +1,182 @@
 import React from 'react'
-import { useForm } from 'react-hook-form';
+
 
 
 
 const Login_page = () => {
 
+const styles = {
+    desktop: {
+      backgroundColor: '#ffffff',
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center', // Center items vertically
+      width: '100%',
+      minHeight: '100vh', // Make the page at least the height of the viewport
+   
+    },
+    div: {
+      backgroundColor: '#ffffff',
+      border: '1px none',
+      height: '100%',
+      position: 'relative',
+      maxWidth: '1440px', // Set maximum width for content
+      width: '100%',
+      padding: '0 20px', // Add horizontal padding for responsiveness
+   
+    },
+    textWrapper: {
+      color: '#000000',
+      fontFamily: 'Inter-SemiBold, Helvetica',
+      fontSize: '96px',
+      fontWeight: 600,
+      letterSpacing: '0',
+      textAlign: 'center', // Center text horizontally
+      margin: '0', // Remove default margins
+    },
+    textWrapper2: {
+      color: '#000000',
+      fontFamily: 'Inter-SemiBold, Helvetica',
+      fontSize: '15px',
+      fontWeight: 600,
+      left: '209px',
+      letterSpacing: '0',
+      lineHeight: 'normal',
+      position: 'absolute',
+      top: '34px',
+      width: '25px',
+    },
+    textWrapper3: {
+      color: '#000000',
+      fontFamily: 'Inter-SemiBold, Helvetica',
+      fontSize: 'px',
+      fontWeight: 600,
+      left: '209px',
+      letterSpacing: '0',
+      lineHeight: 'normal',
+      position: 'absolute',
+      top: '721px',
+      width: '205px',
+    },
+    line: {
+      height: '1px',
+      left: '209px',
+      objectFit: 'cover',
+      position: 'absolute',
+      top: '486px',
+      width: '985px',
+      bordertop: '5px solid black'
+    },
+    img: {
+      height: '2px',
+      left: '209px',
+      objectFit: 'cover',
+      position: 'absolute',
+      top: '863px',
+      width: '997px',
+    },
+    overlapGroup: {
+      backgroundColor: '#ffe81a',
+      borderRadius: '50px',
+      height: '127px',
+      left: '401px',
+      position: 'absolute',
+      top: '1049px',
+      width: '639px',
+    },
+    textWrapper4: {
+      color: '#000000',
+      fontFamily: 'Inter-SemiBold, Helvetica',
+      fontSize: '40px',
+      fontWeight: 600,
+      left: '255px',
+      letterSpacing: '0',
+      lineHeight: 'normal',
+      position: 'absolute',
+      top: '37px',
+      width: '275px',
+    },
+    textWrapper5: {
+      color: '#000000',
+      fontFamily: 'Inter-SemiBold, Helvetica',
+      fontSize: '32px',
+      fontWeight: 600,
+      left: '931px',
+      letterSpacing: '0',
+      lineHeight: 'normal',
+      position: 'absolute',
+      top: '904px',
+      width: '331px',
+    },
+    textWrapper6: {
+      color: '#000000',
+      fontFamily: 'Inter-SemiBold, Helvetica',
+      fontSize: '32px',
+      fontWeight: 600,
+      left: '588px',
+      letterSpacing: '0',
+      lineHeight: 'normal',
+      position: 'absolute',
+      top: '1232px',
+      width: '264px',
+    },
+    googleLogoIcon: {
+      height: '153px',
+      left: '635px',
+      objectFit: 'cover',
+      position: 'absolute',
+      top: '1348px',
+      width: '158px',
+    },
+    textWrapper7: {
+      color: '#bbbbbb',
+      fontFamily: 'Inter-SemiBold, Helvetica',
+      fontSize: '40px',
+      fontWeight: 600,
+      left: '531px',
+      letterSpacing: '0',
+      lineHeight: 'normal',
+      position: 'absolute',
+      top: '413px',
+      whiteSpace: 'nowrap',
+      width: '379px',
+    },
+    textWrapper8: {
+      color: '#bbbbbb',
+      fontFamily: 'Inter-SemiBold, Helvetica',
+      fontSize: '40px',
+      fontWeight: 600,
+      left: '578px',
+      letterSpacing: '0',
+      lineHeight: 'normal',
+      position: 'absolute',
+      top: '798px',
+      whiteSpace: 'nowrap',
+      width: '346px',
+    },
+  };
+
   return (
-    <div className="desktop">
-      <div className="div">
-        <h1 className="text-wrapper">Login</h1>
-        <div className="text-wrapper-2">email</div>
-        <div className="text-wrapper-3">password</div>
-        <img className="line" alt="Line" src="line-1.svg" />
-        <img className="img" alt="Line" src="line-2.svg" />
-        <div className="overlap-group">
-          <div className="text-wrapper-4">Sign Up</div>
+    <div style={styles.desktop}>
+      <div style={styles.div}>
+        <h1 style={styles.textWrapper}>Login</h1>
+        <div style={styles.textWrapper2}>email</div>
+        <div style={styles.textWrapper3}>password</div>
+        <img style={styles.line} alt="Line" src="line-1.svg" />
+        <img style={styles.img} alt="Line" src="line-2.svg" />
+        <div style={styles.overlapGroup}>
+          <div style={styles.textWrapper4}>Sign Up</div>
         </div>
-        <div className="text-wrapper-5">Forgot Password</div>
-        <div className="text-wrapper-6">or Sign Up using</div>
+        <div style={styles.textWrapper5}>Forgot Password</div>
+        <div style={styles.textWrapper6}>or Sign Up using</div>
         <img
-          className="google-logo-icon-png"
+          style={styles.googleLogoIcon}
           alt="Google logo icon png"
           src="google-logo-icon-png-transparent-background-osteopathy-16-1.png"
         />
-        <div className="text-wrapper-7">john@gmail.com</div>
-        <div className="text-wrapper-8">password</div>
+        <div style={styles.textWrapper7}>john@gmail.com</div>
+        <div style={styles.textWrapper8}>password</div>
       </div>
     </div>
   )
